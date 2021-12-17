30522386f71be6661b3af82d2b75344d2773882b
Jalen Greene <33211911+emerald-nomad@users.noreply.github.com>
2021-12-17 08:23:53 -0600

    background image styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,20 @@ import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <picture>
-        <source
-          srcSet="images/home/background-home-mobile.jpg"
-          media="(max-width: 375px)"
-        />
-        <source
-          srcSet="images/home/background-home-tablet.jpg"
-          media="(max-width: 768px)"
-        />
-        <img
-          src="images/home/background-home-desktop.jpg"
-          alt="Earth & Space Background Image"
-        />
-      </picture>
-    </div>
+    <picture className={styles["background"]}>
+      <source
+        srcSet="images/home/background-home-mobile.jpg"
+        media="(max-width: 375px)"
+      />
+      <source
+        srcSet="images/home/background-home-tablet.jpg"
+        media="(max-width: 768px)"
+      />
+      <img
+        src="images/home/background-home-desktop.jpg"
+        alt="Earth & Space Background Image"
+      />
+    </picture>
   );
 };
 
